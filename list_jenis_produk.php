16d4bc7ec69843f0bdf10f00cd64188bc0a356bb
LaurenS999 <74395888+LaurenS999@users.noreply.github.com>
2026-04-10 07:55:55 +0700

    Update list_jenis_produk.php

--- list_jenis_produk.php
+++ list_jenis_produk.php
@@ -1,16 +1,36 @@
 <?php
-	error_reporting(E_ERROR | E_PARSE);
-	require_once 'connectDb.php';
-	$sql = "SELECT * FROM jenis_produk WHERE jenis_produk_hapus = 0 ORDER BY nama_jenis_produk ASC";
-	$result = $c->query($sql);
-	$array = array();
+    error_reporting(E_ERROR | E_PARSE);
+    require_once 'connectDb.php';
 
-	if ($result && $result->num_rows > 0) {
-	    while ($obj = $result->fetch_object()) {
-	        $array[] = $obj;
-	    }
-	}
-	echo json_encode($array);
-	
-	$c->close();
+    $keyword = isset($_GET['keyword']) ? $_GET['keyword'] : '';
+
+    $array = array();
+
+    if (!empty($keyword)) {
+        $sql = "SELECT * FROM jenis_produk 
+                WHERE jenis_produk_hapus = 0 
+                AND nama_jenis_produk LIKE ? 
+                ORDER BY nama_jenis_produk ASC";
+        
+        $stmt = $c->prepare($sql);
+        $searchParam = "%$keyword%";
+        $stmt->bind_param("s", $searchParam);
+        $stmt->execute();
+        $result = $stmt->get_result();
+    } else {
+        $sql = "SELECT * FROM jenis_produk 
+                WHERE jenis_produk_hapus = 0 
+                ORDER BY nama_jenis_produk ASC";
+        $result = $c->query($sql);
+    }
+
+    if ($result && $result->num_rows > 0) {
+        while ($obj = $result->fetch_object()) {
+            $array[] = $obj;
+        }
+    }
+
+    echo json_encode($array);
+    
+    $c->close();
 ?>
